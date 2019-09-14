71a917064aef4910fc6e73f53e9a25649d0e48db
wyd <wangyudong@longshine.com>
2019-09-14 17:18:39 +0800

    1

--- support/ip_store.go
+++ support/ip_store.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"log"
+	"time"
 )
 
 var Db *gorm.DB
@@ -33,7 +34,8 @@ func GetIPs() []*ProxyIp {
 
 func GetFastIPs() *ProxyIp {
 	ip := ProxyIp{}
-	Db.Order("ms asc").First(&ip)
+	Db.Order("last_use_at asc , ms asc").First(&ip)
+	Db.Model(&ip).Update("last_use_at", time.Now())
 	return &ip
 }
 
@@ -41,6 +43,7 @@ type ProxyIp struct {
 	gorm.Model
 	Ip       string `gorm:"unique_index:idx_ip_port"`
 	Port     string `gorm:"unique_index:idx_ip_port"`
+	LastUseAt time.Time
 	Location string
 	Ms       int64
 }
--- support/ip_store_test.go
+++ support/ip_store_test.go
@@ -5,6 +5,7 @@ import (
 	"gopkg.in/resty.v1"
 	"strconv"
 	"testing"
+	"time"
 )
 
 func TestGetIPs(test *testing.T) {
@@ -19,16 +20,24 @@ func TestServer(test *testing.T) {
 }
 
 func TestClient(test *testing.T) {
+	for  {
+		//ipModel := GetFastIPs()
+		host := "http://127.0.0.1:8080"
+		//host := "http://" + ipModel.Ip + ":" + ipModel.Port
+		client := resty.SetProxy(host)
+		client.SetHTTPMode()
+		resp, err := client.R().Get("http://2019.ip138.com/ic.asp")
 
-	client := resty.SetProxy("http://125.108.68.49:9000")
-	client.SetHTTPMode()
-	resp, err := client.R().Get("http://2019.ip138.com/ic.asp")
+		// explore response object
+		fmt.Printf("\nHost: %v", host)
+		fmt.Printf("\nError: %v", err)
+		fmt.Printf("\nResponse Status Code: %v", resp.StatusCode())
+		fmt.Printf("\nResponse Status: %v", resp.Status())
+		fmt.Printf("\nResponse Time: %v", resp.Time())
+		fmt.Printf("\nResponse Received At: %v", resp.ReceivedAt())
+		fmt.Printf("\nResponse Body: %v", resp.String()) // or resp.String() or string(resp.Body())
 
-	// explore response object
-	fmt.Printf("\nError: %v", err)
-	fmt.Printf("\nResponse Status Code: %v", resp.StatusCode())
-	fmt.Printf("\nResponse Status: %v", resp.Status())
-	fmt.Printf("\nResponse Time: %v", resp.Time())
-	fmt.Printf("\nResponse Received At: %v", resp.ReceivedAt())
-	fmt.Printf("\nResponse Body: %v", resp) // or resp.String() or string(resp.Body())
+		time.Sleep(1 * time.Second)
+	}
+
 }
